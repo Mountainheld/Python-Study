396090d3a849fe8485671f597b864d7a54b69116
Mountainheld <hyang9287@gmail.com>
2019-07-17 18:56:19 +0800

    update at 2019-07-17

---
+++ leetcode_solutions/0059-spiral-matrix-ii/spiral-matrix-ii.py
@@ -0,0 +1,27 @@
+# Given a positive integer n, generate a square matrix filled with elements from 1 to n2 in spiral order.
+#
+# Example:
+#
+#
+# Input: 3
+# Output:
+# [
+#  [ 1, 2, 3 ],
+#  [ 8, 9, 4 ],
+#  [ 7, 6, 5 ]
+# ]
+#
+#
+
+
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        
+        A = []
+        lo = n*n + 1
+        while lo > 1:
+            
+            lo, hi = lo - len(A), lo
+            A = [list(range(lo,hi))] + [*zip(*A[::-1])]
+            
+        return [list(i) for i in A]
---
+++ leetcode_solutions/0083-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.py
@@ -0,0 +1,45 @@
+# Given a sorted linked list, delete all duplicates such that each element appear only once.
+#
+# Example 1:
+#
+#
+# Input: 1->1->2
+# Output: 1->2
+#
+#
+# Example 2:
+#
+#
+# Input: 1->1->2->3->3
+# Output: 1->2->3
+#
+#
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        
+        p = head
+        
+        if not p or not p.next: return head
+        
+        while p.next:
+            
+            if p.val == p.next.val:
+                if p.next.next:
+                    p.next = p.next.next
+                else:
+                    p.next = None
+            else:
+                p = p.next
+                    
+        return head
+                    
+                
+                
